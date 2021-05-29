20dd51c07f4bdbf024e79e3894fa0261f8d9b610
fghikl123 <84986576+fghikl123@users.noreply.github.com>
2021-05-29 13:08:59 +0530

    Update app.py
    hope this change helps others to name it as their projects

--- app.py
+++ app.py
@@ -55,4 +55,3 @@ else:
     if st.button("Run"):
         generate_output(text)
 
-st.markdown("<br><br><hr><center>Made with ❤️ by <a href='https://prateekralhan.github.io/'><strong>Prateek Ralhan</strong></a></center><hr>", unsafe_allow_html=True)
